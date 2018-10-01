b653e42a80589d16173ed990527c0b76b6f3de4e
Victoria Pavlova <vpavlova@systemorph.com>
2018-10-01 13:17:56 +0300

    Contact page

--- inc/meta-boxes.php
+++ inc/meta-boxes.php
@@ -21,3 +21,10 @@ function the_systemorph_contact_info() {
 	the_systemorph_contact_phone();
 	the_systemorph_contact_email();
 }
+
+function the_systemorph_page_subtitle() {
+	$rwmbMeta = rwmb_meta( 'page_settings_subtitle');
+	if ($rwmbMeta) :
+ 		echo "<h2 class=\"page-subtitle\">$rwmbMeta</h2>";
+ 	endif;
+}
