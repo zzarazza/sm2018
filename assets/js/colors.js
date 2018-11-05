af670717ee37dbf1875b02a5de2a6f714c393781
Victoria Pavlova <vpavlova@systemorph.com>
2018-11-05 23:50:28 +0300

    Update

---
+++ assets/js/colors.js
@@ -0,0 +1,69 @@
+// Colors
+
+const smBgColors = [
+	{
+		name  : 'Primary',
+		slug  : 'primary',
+		color : '#101f30'
+	},
+	{
+		name  : 'Secondary',
+		slug  : 'secondary',
+		color : '#2e3641'
+	},
+	{
+		name  : 'SM blue',
+		slug  : 'sm-blue',
+		color : '#009cde'
+	},
+	{
+		name  : 'SM purple',
+		slug  : 'sm-purple',
+		color : '#623a81'
+	},
+	{
+		name  : 'Gray',
+		slug  : 'gray',
+		color : '#a6a6a6'
+	},
+	{
+		name  : 'Lavender gray',
+		slug  : 'lavender-gray',
+		color : '#a9a7b3'
+	},
+	{
+		name  : 'Blue',
+		slug  : 'blue',
+		color : '#285fae'
+	},
+	{
+		name  : 'Red',
+		slug  : 'alert',
+		color : '#cc1300'
+	}
+];
+
+const smIconColors = [
+	{
+		name  : 'Transparent white',
+		slug  : 'tr-white',
+		color : '#f0f0f0'
+	},
+	{
+		name  : 'White',
+		slug  : 'white',
+		color : '#ffffff'
+	},
+	{
+		name  : 'Primary',
+		slug  : 'primary',
+		color : '#101f30'
+	}
+];
+
+const smIcons = [
+    { label: 'Folder', value: 'folder' },
+    { label: 'User', value: 'user' },
+    { label: 'Users', value: 'users' },
+    { label: 'Chart', value: 'chart' },
+];
