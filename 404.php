b8053854a00b3e69f0a25676c91b6375a66a1ed7
Victoria Pavlova <vpavlova@systemorph.com>
2018-11-12 04:02:31 +0300

    404

--- 404.php
+++ 404.php
@@ -16,16 +16,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'systemorph' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'systemorph' ); ?></p>
+			<section class="error-404 not-found wp-block-sm-section">
 
-					<?php get_search_form(); ?>
+				<h1 class="page-title"><?php _e( 'It doesn\'t look like anything to me.', 'systemorph' ); ?></h1>
+				<p><?php _e( 'These violent delights have violent ends.', 'systemorph' ); ?></p>
 
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
