a5c6274ede792c1a96eca88ebfaf7e4d8d4029e7
Victoria Pavlova <vpavlova@systemorph.com>
2018-11-28 16:54:16 +0300

    Icons

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -19,7 +19,7 @@ if ( ! function_exists( 'systemorph_posted_on' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author */
 			__( 'by %s', 'systemorph' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span> '
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author_meta( 'display_name' ) . '</a></span> '
 		);
 
 		// Finally, let's write all of this to the page.
