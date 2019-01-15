5a36b54e8a89d0fb3b5a7fa6a58f4fa9620ee409
Victoria Pavlova <vpavlova@systemorph.com>
2019-01-16 02:01:52 +0300

    Paging

--- index.php
+++ index.php
@@ -20,11 +20,11 @@ get_header(); ?>
 <div class="wrap">
 	<?php if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
+			<h1 class="page-title"><?php _e( 'The Systemorph Blog', 'systemorph' ); ?></h1>
 		</header>
 	<?php else : ?>
 	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'The Systemorph Blog', 'systemorph' ); ?></h2>
+		<h2 class="page-title"><?php _e( 'Posts', 'systemorph' ); ?></h2>
 	</header>
 	<?php endif; ?>
 
