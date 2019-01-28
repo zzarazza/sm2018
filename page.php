f6fded2ac744b2b604a336c3b2a3c5a8c72ba48f
Victoria Pavlova <victoria@convert.no>
2019-01-28 12:30:17 +0300

    Removed file upload

--- page.php
+++ page.php
@@ -21,9 +21,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<?php
-				the_systemorph_page_attachment( get_the_ID() );
-			?>
-			<?php
 			while ( have_posts() ) :
 				the_post();
 
