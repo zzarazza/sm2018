14d71e21eee22fcacce010c018d356c3bba99fcc
Victoria Pavlova <victoria@convert.no>
2019-01-28 11:29:15 +0300

    Meta

--- inc/meta-boxes.php
+++ inc/meta-boxes.php
@@ -175,6 +175,7 @@ if ( ! function_exists( 'get_systemorph_author_title' ) ) :
 		$output = "";
 
 		$meta = rwmb_the_value( 'user_meta_title',  array( 'object_type' => 'user' ), $post->post_author, false );
+		var_dump($meta);
 
 		if ($meta) :
 			$output .= $meta . ' ';
