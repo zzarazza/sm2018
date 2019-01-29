f254c09724d15f14f5ae37aa8e78ecbb574d952b
Victoria Pavlova <vpavlova@systemorph.com>
2019-01-30 01:21:30 +0300

    Enabling reCaptcha on case studies

--- inc/recaptcha.php
+++ inc/recaptcha.php
@@ -7,7 +7,9 @@ if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
 }
 
 function oiw_load_recaptcha_badge_page(){
-    if ( !is_page( array( 'contact', 'white-paper', 'swiss-re', 'zurich-insurance' ) ) ) {
+    if ( !is_page( array( 'contact', 'white-paper' ) ) ||
+    	 !is_single ( array( 'swiss-re', 'zurich-insurance' ) )
+		) {
         wp_dequeue_script('google-recaptcha');
     }
 }
