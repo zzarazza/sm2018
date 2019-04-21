cae55d0fefdca8f397d27669794f4b018919c57d
Victoria Pavlova <vpavlova@systemorph.com>
2019-04-21 17:35:57 +0300

    Excerpt length

--- functions.php
+++ functions.php
@@ -676,7 +676,7 @@ function sm_trim_excerpt( $text = '' ) {
     $text = strip_shortcodes( $text );
     $text = apply_filters('the_content', $text);
     $text = str_replace(']]>', ']]&gt;', $text);
-    $excerpt_length = apply_filters('excerpt_length', 75);
+    $excerpt_length = apply_filters('excerpt_length', 83);
     $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
     return wp_trim_words( $text, $excerpt_length, $excerpt_more );
 }
