ee9a73399fd063236617837aae6b5c97a7e03730
Victoria Pavlova <victoria@convert.no>
2019-05-02 00:34:21 +0300

    IFRS 17 Template name change

--- page-ifrs-17-compliance.php
+++ page-ifrs-17-compliance.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * Template Name: IFRS 17
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
